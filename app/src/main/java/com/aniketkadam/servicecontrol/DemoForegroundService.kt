0c8c94ee178e184122afec333b4f61d81a3c08d0
Aniket Kadam <anikadamg@gmail.com>
2018-11-27 22:37:22 +0530

    Added DemoForegroundService

---
+++ app/src/main/java/com/aniketkadam/servicecontrol/DemoForegroundService.kt
@@ -0,0 +1,12 @@
+package com.aniketkadam.servicecontrol
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class DemoForegroundService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
