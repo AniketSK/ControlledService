558685336776732660527c5e97f9692797419a2e
Aniket Kadam <anikadamg@gmail.com>
2018-11-27 22:52:35 +0530

    Added a empty presenter that just declares required functions but does'nt implement them.

---
+++ app/src/main/java/com/aniketkadam/servicecontrol/ActivityPresenter.kt
@@ -0,0 +1,13 @@
+package com.aniketkadam.servicecontrol
+
+class ActivityPresenter : Presenter {
+    override fun onStart() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onStop(isFinishing: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
