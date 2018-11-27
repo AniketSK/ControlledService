19720a94e5bfa1e54c18a0b937859e62dfe303ce
Aniket Kadam <anikadamg@gmail.com>
2018-11-27 23:12:05 +0530

    Adding responsibilities to the view to begin the service. Enforcing that the view is passed in, in onStart

--- app/src/main/java/com/aniketkadam/servicecontrol/MainActivityContract.kt
+++ app/src/main/java/com/aniketkadam/servicecontrol/MainActivityContract.kt
@@ -4,12 +4,13 @@ import io.reactivex.Observable
 
 interface MainActivityContract {
     interface Presenter {
-        fun onStart()
+        fun onStart(view: View)
         fun onStop(isFinishing: Boolean)
     }
 
     interface View {
         fun switchToggle(): Observable<Boolean>
         fun notificationButtonClick(): Observable<Unit>
+        fun serviceRun(boolean: Boolean)
     }
 }
