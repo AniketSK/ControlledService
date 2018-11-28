31f48dc0dd237ef83ca3727d8c43448e1e34e5fb
Aniket Kadam <anikadamg@gmail.com>
2018-11-28 19:53:28 +0530

    Implemented stopping, starting and adding a notification for the activity and service. Still got a bit of work to do on actually showing and hiding the notification.

--- app/src/main/java/com/aniketkadam/servicecontrol/demoactivity/MainActivity.kt
+++ app/src/main/java/com/aniketkadam/servicecontrol/demoactivity/MainActivity.kt
@@ -3,8 +3,11 @@ package com.aniketkadam.servicecontrol.demoactivity
 import android.os.Bundle
 import com.aniketkadam.servicecontrol.R
 import com.aniketkadam.servicecontrol.base.BaseActivity
+import com.aniketkadam.servicecontrol.base.extensions.exhaustive
+import com.aniketkadam.servicecontrol.base.models.Running
 import com.aniketkadam.servicecontrol.base.models.ServiceStates
 import com.aniketkadam.servicecontrol.base.mvp.IBasePresenter
+import com.aniketkadam.servicecontrol.demoservice.DemoForegroundService
 import com.jakewharton.rxbinding3.widget.checkedChanges
 import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,9 +15,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : BaseActivity<ActivityPresenter>(), MainActivityContract.View {
     override fun switchToggle(): Observable<Boolean> = serviceActivationSwitch.checkedChanges()
 
-    override fun serviceRun(serviceStates: ServiceStates) {
-        
-    }
+    override fun serviceRun(serviceStates: ServiceStates) = when (serviceStates) {
+        ServiceStates.NotRunning -> DemoForegroundService.stopService(this)
+        is Running -> {
+            DemoForegroundService.startService(this)
+            DemoForegroundService.manageNotification(this, serviceStates)
+        }
+    }.exhaustive
 
     override fun getPresenter(): IBasePresenter {
         return ActivityPresenter()
--- app/src/main/java/com/aniketkadam/servicecontrol/demoservice/DemoForegroundService.kt
+++ app/src/main/java/com/aniketkadam/servicecontrol/demoservice/DemoForegroundService.kt
@@ -1,12 +1,35 @@
 package com.aniketkadam.servicecontrol.demoservice
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
+import com.aniketkadam.servicecontrol.base.models.Running
 
 class DemoForegroundService : Service() {
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
+    }
+
+    companion object {
+        fun stopService(c: Context) {
+            c.stopService(Intent(c, DemoForegroundService::class.java))
+        }
+
+        fun startService(c: Context) {
+            c.startService(Intent(c, DemoForegroundService::class.java))
+        }
+
+        fun manageNotification(c: Context, running: Running) {
+            val done = when (running) {
+                Running.NotificationVisible -> showNotification(c, true)
+                Running.NotificationHidden -> showNotification(c, false)
+            }
+        }
+
+        private fun showNotification(c: Context, show: Boolean) {
+            TODO("got to implement actually showing and hiding the notification")
+        }
     }
 }
